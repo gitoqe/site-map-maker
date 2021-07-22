5c9450a9cc4a847e08de845f8a840a1c362419a4
Maxim <ififorgetyou@gmail.com>
2021-07-22 18:04:01 +0300

    add html parse into console

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-let enterArgs = process.argv[2].toLowerCase();
-let patternRegExpURL = (process.argv[3]) ? process.argv[3] : /http[s]{0,1}:\/\/[a-z0-9]*.[a-z0-9]*.[a-z]{2,3}\//;
+const http = require("http"); // requests
 
-if (patternRegExpURL.test(enterArgs)) {
-  console.log(`✔ correct URL: ${enterArgs}`);
-  //const targetURL = new URL(enterArgs);
+let URL = process.argv[2].toLowerCase();
+let patternRegExpURL = process.argv[3]
+  ? process.argv[3]
+  : /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+if (patternRegExpURL.test(URL)) {
+  console.log(`✔ correct URL: ${URL}`);
+  //const targetURL = new URL(URL);
 } else {
-  console.log(`✖ incorrect URL: ${enterArgs}`);
-  console.log(`mask is: HTTP(s)://abcd.efg.xyz/`)
+  console.log(`✖ incorrect URL: ${URL}`);
+  console.log(`Mask is: HTTP(s)://abcd.efg.xyz/`);
+  console.log(`You can set your own regexp as second argument`);
   return;
 }
-console.log(enterArgs);
+
+// https://nodejs.dev/learn/making-http-requests-with-nodejs
+const options = {
+  hostname: URL,
+  path: '/',
+  method: 'GET'
+}
+
+const req = http.request(options, res => {
+  console.log(`status code: ${res.statusCode}`)
+
+  res.on('data', d=> {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', er => {
+  console.error(er)
+})
+
+req.end()
