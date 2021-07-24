e27fcaba724cd2ed1bd94c63e5b6a2c4b526f609
Maxim <ififorgetyou@gmail.com>
2021-07-24 23:18:30 +0300

    chor httprequest, filesave

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ let patternRegExpURL = process.argv[3]
 
 if (patternRegExpURL.test(URL)) {
   console.log(`[✔] Correct URL: ${URL}`);
-  //const targetURL = new URL(URL);
 } else {
   console.log(`[✖] incorrect URL: ${URL}`);
   console.log(`Mask is: HTTP(s)://abcd.efg.xyz/`);
@@ -17,33 +16,72 @@ if (patternRegExpURL.test(URL)) {
 }
 
 // https://nodejs.dev/learn/making-http-requests-with-nodejs
+// options for request
 const options = {
   hostname: URL,
   path: "/",
   method: "GET",
 };
 
-let u = 0;
-const req = http.request(options, (res) => {
-  console.log(`Status code: ${res.statusCode}`);
-  let rawHtml = "";
-  res.on("data", (d) => {
-    rawHtml += d;
-    process.stdout.write(d);
+/**
+ * Make HTTP request and get 
+ * @param {object} params Parameters for HTTP request
+ * @returns Promise
+ */
+function httpRequest(params) {
 
-    fs.writeFile(`${__dirname}/parseResults/raw`, rawHtml, (err) => {
-      if (err) {
-        return console.log(`[✖] error with save: ${err}`);
-      } else {
-        console.log(`[✔] File saved as: ${__dirname}/parseResults/raw`);
-        console.log(u++);
+  console.log("]>>>> FUNCTION");
+  return new Promise((resolve, reject) => {
+
+    console.log(">]>>> PROMISE");
+    const req = http.request(params, (res) => {
+
+      console.log(">>]>> REQUEST");
+      // check response code
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        // error code - reject promise
+        return reject(new Error(`Error status code: ${res.statusCode}`));
       }
+
+      // saving data
+      let rawData = "";
+      res.on("data", (dataChunk) => {
+        console.log(">>>]> DATA OBTAINING");
+        rawData += dataChunk;
+      });
+
+      // resolving on end
+      res.on("end", () => {
+        try {
+          //console.log(rawData);
+          //rawData = JSON.parse(rawData.toString());
+          console.log(">>>>] DATA PARsING");
+        } catch (error) {
+          // error -> reject with error
+          reject(error);
+        }
+        // parsed successfully -> return resolve
+        resolve(rawData);
+      });
     });
+
+    // if we have error with request
+    req.on("error", (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+}
+
+httpRequest(options).then((result) => {
+  console.log(">>>>>]]]] FIN :O");
+  fs.writeFile(`${__dirname}/parseResults/raw`, result, (err) => {
+    if (err) {
+      return console.log(`[✖] error with save: ${err}`);
+    } else {
+      console.log(`[✔] File saved as: ${__dirname}/parseResults/raw`);
+      //console.log(informationChunk++);
+    }
   });
 });
-
-req.on("error", (er) => {
-  console.error(er);
-});
-
-req.end();
