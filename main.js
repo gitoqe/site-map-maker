0cbc81ec83ece8d9bb6dab589894dd032423f582
Maxim Sergienko <creativesun@yandex.ru>
2022-06-04 01:05:06 +0300

    feat(main): Tree and TreeNode classes

--- main.js
+++ main.js
@@ -78,19 +78,77 @@ class Tree {
         this.root = new TreeNode(key, value);
     }
 
+    /**
+     * 
+     * @param {*} node 
+     */
+    *preOrderBypass(node = this.root) {
+        yield node;
+        if (node.children.length) {
+            for (const child of node.children) {
+                yield* this.preOrderBypass(child);
+            }
+        }
+    }
 
-
-    // insert() {
-
-    // }
-
-    // remove() {
-
-    // }
-
-    // find () {
+    /**
+     * Traverses the tree by recursively traversing each node's children followed by the node
+     * @param {*} node 
+     */
+    *postOrderBypass(node = this.root) {
+        if (node.children.length) {
+            for (const child of node.children) {
+                yield* this.postOrderBypass(child);
+            }
+        }
+        yield node;
+    }
         
-    // }
+
+    /**
+     * 
+     * @param {*} parentNodeKey
+     * @param {*} key 
+     * @param {*} value 
+     * @return {bool}
+     */
+    insert(parentNodeKey, key, value = key) {
+        for (const node of this.preOrderBypass()) {
+            if (node.key === parentNodeKey) {
+                node.children.push(new TreeNode(key, value, node));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @param {*} key 
+     * @return {*}
+     */
+    remove(key) {
+        for (const node of this.preOrderBypass()) {
+            const filtered = node.children.filter(c => c.key !== key);
+            if (filtered.length !== node.children.length) {
+                node.children = filtered;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * 
+     * @param {*} key 
+     * @return {*} 
+     */
+    find(key) {
+        for (const node of this.preOrderBypass()) {
+            if (node.key === key) return node;
+        }
+        return undefined;
+    }
 }
 
 main(URL, DEPTH);
